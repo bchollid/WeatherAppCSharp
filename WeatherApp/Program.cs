355a3d449d4f0fcabf015d8db0d7da71ad6cad1d
bchollid <brandonholliday1@gmail.com>
2024-02-02 15:57:34 -0700

    Finished

---
+++ WeatherApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json.Linq;
+
+namespace WeatherApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var key = File.ReadAllText("appsettings.json");
+            var APIKey = JObject.Parse(key).GetValue("APIKey").ToString();
+
+            Console.WriteLine("Please enter your city name.");
+            var cityName = Console.ReadLine();
+
+            var apiCall = $"https://api.openweathermap.org/data/2.5/weather?q={cityName}&units=imperial&appid={APIKey}";
+            Console.WriteLine();
+            Console.WriteLine($"It is currently {WeatherInfo.GetTemp(apiCall)} degrees Farenheit in {cityName}.");
+        }
+    }
+}
